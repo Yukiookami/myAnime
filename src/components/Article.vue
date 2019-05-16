37ad81fc4262fc26b340860f6bbde1f865e3dee9
dantecsm <fakersfather@qq.com>
2019-05-16 11:56:34 +0800

    为 Detail, Guide, Message, Unzip 添加 Article 组件 (add Article.vue)

---
+++ src/components/Article.vue
@@ -0,0 +1,280 @@
+<template>
+  <div class="Article" data-aos="fade-up" data-aos-duration="1500">
+    <div class="title-wrapper">
+      <h1 class="title">1/2 summer</h1>
+    </div>
+    <ul class="tags">
+      <li class="tag"><i class="el-icon-date"></i>2019-05-12</li>
+      <li class="tag"><i class="el-icon-view"></i>47,466℃</li>
+    </ul>
+    <div class="screenshot">
+      <blockquote>游戏截图</blockquote>
+      <ul>
+        <li><img src="../assets/image/s1.jpg" alt=""></li>
+        <li><img src="../assets/image/s2.jpg" alt=""></li>
+        <li><img src="../assets/image/s3.jpg" alt=""></li>
+        <li><img src="../assets/image/s4.jpg" alt=""></li>
+        <li><img src="../assets/image/s5.jpg" alt=""></li>
+      </ul>
+      <p>
+        <br><br><br><br>
+      </p>
+    </div>
+    <div class="introduce">
+      <blockquote>游戏简介</blockquote>
+      <div class="article">
+        春日烂漫。于相遇和恋爱中悄然躁动的这个世间。<br>
+        对于世间那玫瑰色氛围完全不在意呢。<br>
+        今日也一如往常地、在怠惰之路上一往无前的主人公・花咲游真。<br>
+        虽然今天也想继续吊儿郎当地度过……<br>
+        「欢迎来到幽灵部！从今天开始你就是部长哟」<br>
+        聚集了幽灵部员的社团所进行的活动到底是什么玩意。<br>
+        明明至今都不知有这么一个社团存在、突然就被任命为部长是在搞什么飞机!?<br>
+        「请别在意我。没有和你友好相处的打算」<br>
+        在周围制造交流屏障放出冷言冷语的她是、一匹狼的少女・不知火祈（しらぬい いのり）。<br>
+        「请让我充分享受快乐吧！新部长♪」<br>
+        把我扯入这意义不明的幽灵部的罪魁祸首，以校内最长留级年数自满的玖音彩乃（くおん あやの）说出了毫无责任的话。<br>
+        「我喜欢你。从第一次见面起就！感、感到恶心吗？」<br>
+        对我一见钟情是怎么回事？突然就告白的这个孩子是琴吹光（ことぶき ひかり）。<br>
+        「加入分类委员绝对会很有趣哦。所以来我们这边吧、吶！」<br>
+        完全不在意我心情地劝诱我的是、身为恶友的空森若叶（そらもり わかば）。<br>
+        不知不觉中已经被包围了、被一群难以应付的女孩子们。<br>
+        「这是个好机会啊。你也别总是懒散度日了试试享受青春吧」<br>
+        那么会和主人公・花咲游真结缘的到底是……？<br>
+        即使无聊透顶、那不可替代的青春物语、如今于此开演！<br>
+      </div>
+      <p><br><br></p>
+    </div>
+    <div class="staff">
+      <blockquote>汉化STAFF</blockquote>
+      <div class="article">
+        组长: 邓格拉斯<br>
+        副组长: 乾纱凪<br>
+        审核: 零白夜<br>
+        坑主: 桂雪葉（已跑路）<br>
+        程序: dwing<br>
+        初翻: 沙罗双树 白云悠悠 蔻蔻 ArieiS 坚果 圣诞老人 沙罗双树 C酱 宁静 言笑 野生萝莉控 宁静<br>
+        言笑 麦にオオカミ 老嘞个贱 二次元丶殤 百页 SilentMing 路过某饼 真优 闪耀之风 可能不是花卷<br>
+        Croky 纱凪/xdfreedam 雪花 Djakrista 邓格拉斯 御坂小喵 DOGE 二两 KillerQueen 如月酱<br>
+        润色: 绵羊的神话 苍蓝钢铁的91 闲云 Jsok MisakiShigure lampy 燹燏 Sora 闲狐兔<br>
+        mooninsiderain 绵阳的神话 二次元丶殇 小F 小黑 社长<br>
+        校对: SilentMing 炎俱土 星野夏夜 圣诞老人 老嘞个贱 邓格拉斯 xdfreedam 言笑 娜鹿鹿<br>
+        晴岚 Doge 沙罗双树 工口组长 毛巾 樱花夏 アリババ Djakrista 来者何人<br>
+        二校: 千夜 Jsc6924 梓洋 JC ArieiS 夏露 莫辰 熹 路过某饼<br>
+        三校: Jsc6924 千夜 夏露 梓洋 橘子 莫辰 lateP<br>
+        美工: 图书管理员酱<br>
+        测试: 乾纱凪 ACG-forever！&城彩学园全体成员<br>
+        特别感谢: 信子御津<br>
+      </div>
+      <p><br></p>
+    </div>
+    <div class="share">
+      <header>百度网盘链接</header>
+      <main>
+        <el-button>链接</el-button>
+      </main>
+      <footer>
+        <p><span>提取密码：</span>A562</p>
+        <p><span>备注：</span>已打BugBug追加剧本「お兄ちゃんとののかのヒミツの保健体育」补丁；已打【城彩学园汉化组】《花咲ワークスプリング！》正式汉化补丁V1.2017.4.10</p>
+      </footer>
+    </div>
+    <div class="md5">
+      <div class="article">
+        文件MD5:<br>
+        part1 MD5: 445D9866EBAC40B2FA5E3801A5DD6BD7
+        part2 MD5: 075616DA4FC30D1CBACE066EFF3CFB31
+        part3 MD5: 58EA4C2F52E4454F5E7EBE41A06B55EC
+        part4 MD5: 880C8D4B69483164D4755395EA574AE7
+        <br>总大小：3.26 GB[rr10%] 解压后：3.52 GB
+        <br>任何解压相关问题请看网站顶部解压必读，不看而发问直接小黑屋
+      </div>
+      <p><br></p>
+    </div>
+    <div class="blogTag">
+      <i class="el-icon-price-tag"></i>
+      <router-link to="/tag/1234">
+        <el-tag color="#5cb85c">纯爱</el-tag>
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Article"
+  }
+</script>
+
+<style scoped lang="less">
+  .Article {
+    position: relative;
+    padding: 20px;
+    margin-bottom: 30px;
+    background-color: rgba(230, 238, 232, .5);
+    box-shadow: 0px 0px 8px #000;
+
+    transition-timing-function: cubic-bezier(.175, .885, .32, .1 .275);
+    border-radius: 10px;
+    z-index: 10;
+
+    &:hover {
+      transition: all .35s ease-in-out;
+      box-shadow: 0px 0px 50px #000;
+    }
+
+    .title-wrapper {
+      text-align: center;
+
+      .title {
+        color: #fff;
+        background: #1ec7e6;
+        display: inline-block;
+        font: 28px '微软雅黑', Verdana, Arial, Helvetica, sans-serif;
+        padding: 2px;
+        margin: 2px auto;
+        opacity: .8;
+        line-height: 1.5;
+        border-radius: 5px;
+
+        &:hover {
+          opacity: 1;
+          background-color: rgba(0, 0, 0, 0);
+          animation: neno 1.5s ease-in-out infinite alternate;
+        }
+
+        @keyframes neno {
+          0% {
+            text-shadow: 0 0 10px #fff, 0 0 20px #fff, 0 0 30px #fff, 0 0 40px #228DFF, 0 0 70px #228DFF, 0 0 80px #228DFF, 0 0 100px #228DFF, 0 0 150px #228DFF;
+          }
+          100% {
+            text-shadow: 0 0 5px #fff, 0 0 10px #fff, 0 0 15px #fff, 0 0 20px #228DFF, 0 0 35px #228DFF, 0 0 40px #228DFF, 0 0 50px #228DFF, 0 0 75px #228DFF;
+          }
+        }
+      }
+    }
+
+    .tags {
+      margin: 20px 0;
+      text-align: center;
+
+      .tag {
+        display: inline-block;
+        margin: 0 .15em;
+        padding: .2em .6em .3em;
+        border-radius: .25em;
+        line-height: 1;
+        color: #fff;
+        font-size: 13px;
+        font-weight: bold;
+        background-color: rgba(10, 10, 0, .7);
+
+        i {
+          margin-right: .15em;
+        }
+      }
+    }
+
+    blockquote {
+      border-color: rgb(0, 191, 255);
+      background-color: rgba(255, 255, 255, .51);
+      padding: 10px 20px;
+      margin: 0 0 20px;
+      border-left: 5px solid rgb(0, 191, 255);
+      text-align: left;
+      font-size: 12pt;
+      font-weight: 800;
+      color: #6699FF;
+    }
+
+    .article {
+      background-color: #dff0d8;
+      color: #468847;
+      font-size: 12px;
+      padding: 1em 1.5em;
+      border-radius: 4px;
+    }
+
+    .screenshot {
+      img {
+        width: 650px;
+
+        &:hover {
+          transition: all .3s ease-in-out;
+          transform: scale(1.5);
+          cursor: zoom-in;
+        }
+      }
+    }
+
+    .share {
+      margin-bottom: 20px;
+      border-right: 4px;
+      background-color: rgba(230, 238, 232, .5);
+      border-radius: 4px;
+      overflow: hidden;
+      &:hover {
+        box-shadow: 0 0 50px rgb(0, 0, 0);
+      }
+
+      header {
+        background-color: #428bca;
+        color: #fff;
+        font-size: 14px;
+        line-height: 14px;
+        padding: 10px 15px;
+      }
+
+      main {
+        padding: 15px;
+
+        .el-button {
+          background-color: #d9534f;
+          border: none;
+          transition: all .25s ease-in-out;
+          color: #fff;
+          font-size: 12px;
+          padding: 6px 12px;
+          line-height: 1.5;
+          border-radius: 4px;
+
+          &:hover {
+            background-color: #d2322d;
+          }
+        }
+      }
+
+      footer {
+        background-color: #fff;
+        padding: 10px 15px;
+        font-size: 10pt;
+        font-weight: bold;
+        color: #f00;
+
+        span {
+          color: #3d4450;
+        }
+      }
+    }
+
+    .md5 {
+      .article {
+        color: #3a87ad;
+        background-color: rgba(217, 237, 247, .8);
+      }
+    }
+
+    .blogTag {
+      i {
+        margin-right: .5em;
+      }
+
+      a {
+        opacity: .7;
+
+        &:hover {
+          opacity: 1;
+        }
+      }
+    }
+  }
+</style>
--- src/pages/Message/template.vue
+++ src/pages/Message/template.vue
@@ -2,9 +2,10 @@
   <div class="Message">
     <section class="content-wrapper">
       <div class="posts-wrapper">
-        <div class="post-item" data-aos="zoom-in-up" data-aos-duration="500">
-          这是留言板
-        </div>
+<!--        <div class="post-item" data-aos="zoom-in-up" data-aos-duration="500">-->
+<!--          这是留言板-->
+<!--        </div>-->
+        <Article></Article>
       </div>
       <div class="comment-wrapper">
         <Comments></Comments>
@@ -19,10 +20,11 @@
 <script>
   import Besider from '../../components/Besider'
   import Comments from '../../components/Comments'
+  import Article from '../../components/Article'
 
   export default {
     name: 'Message',
-    components: {Besider, Comments}
+    components: {Besider, Comments, Article}
   }
 </script>
 
@@ -35,201 +37,6 @@
     section.content-wrapper {
       flex: 2;
       padding: 0 15px;
-
-      .breadcrumb-wrapper {
-        display: flex;
-        align-items: center;
-        background-color: @paneColor;
-        box-shadow: 0 0 5px #c2c2c2;
-        margin-bottom: 40px;
-        border-radius: 4px;
-
-        i {
-          font-size: 12px;
-          padding: 1em .5em;
-        }
-
-        .el-breadcrumb {
-          padding: 8px 15px;
-          font-size: 12px;
-          color: rgb(210, 50, 45);
-
-          #current {
-            color: rgb(61, 68, 80);
-          }
-        }
-      }
-
-      .posts-wrapper {
-        .post-item {
-          position: relative;
-          padding: 20px;
-          margin-bottom: 30px;
-          background-color: rgba(230, 238, 232, .5);
-          box-shadow: 0px 0px 8px #000;
-
-          transition-timing-function: cubic-bezier(.175, .885, .32, .1 .275);
-          border-radius: 10px;
-          z-index: 10;
-
-          &:hover {
-            transition: all .35s ease-in-out;
-            box-shadow: 0px 0px 50px #000;
-          }
-
-          .title-wrapper {
-            text-align: center;
-
-            .title {
-              color: #fff;
-              background: #1ec7e6;
-              display: inline-block;
-              font: 28px '微软雅黑', Verdana, Arial, Helvetica, sans-serif;
-              padding: 2px;
-              margin: 2px auto;
-              opacity: .8;
-              line-height: 1.5;
-              border-radius: 5px;
-
-              &:hover {
-                opacity: 1;
-                background-color: rgba(0, 0, 0, 0);
-                animation: neno 1.5s ease-in-out infinite alternate;
-              }
-
-              @keyframes neno {
-                0% {
-                  text-shadow: 0 0 10px #fff, 0 0 20px #fff, 0 0 30px #fff, 0 0 40px #228DFF, 0 0 70px #228DFF, 0 0 80px #228DFF, 0 0 100px #228DFF, 0 0 150px #228DFF;
-                }
-                100% {
-                  text-shadow: 0 0 5px #fff, 0 0 10px #fff, 0 0 15px #fff, 0 0 20px #228DFF, 0 0 35px #228DFF, 0 0 40px #228DFF, 0 0 50px #228DFF, 0 0 75px #228DFF;
-                }
-              }
-            }
-          }
-
-          .tags {
-            margin: 20px 0;
-            text-align: center;
-
-            .tag {
-              display: inline-block;
-              margin: 0 .15em;
-              padding: .2em .6em .3em;
-              border-radius: .25em;
-              line-height: 1;
-              color: #fff;
-              font-size: 13px;
-              font-weight: bold;
-              background-color: rgba(10, 10, 0, .7);
-
-              i {
-                margin-right: .15em;
-              }
-            }
-          }
-
-          blockquote {
-            border-color: rgb(0, 191, 255);
-            background-color: rgba(255, 255, 255, .51);
-            padding: 10px 20px;
-            margin: 0 0 20px;
-            border-left: 5px solid rgb(0, 191, 255);
-            text-align: left;
-            font-size: 12pt;
-            font-weight: 800;
-            color: #6699FF;
-          }
-
-          .article {
-            background-color: #dff0d8;
-            color: #468847;
-            font-size: 12px;
-            padding: 1em 1.5em;
-            border-radius: 4px;
-          }
-
-          .screenshot {
-            img {
-              width: 650px;
-
-              &:hover {
-                transition: all .3s ease-in-out;
-                transform: scale(1.5);
-                cursor: zoom-in;
-              }
-            }
-          }
-
-          .share {
-            margin-bottom: 20px;
-            border-right: 4px;
-            background-color: rgba(230, 238, 232, .5);
-            border-radius: 4px;
-            overflow: hidden;
-
-            header {
-              background-color: #428bca;
-              color: #fff;
-              font-size: 14px;
-              line-height: 14px;
-              padding: 10px 15px;
-            }
-
-            main {
-              padding: 15px;
-
-              .el-button {
-                background-color: #d9534f;
-                border: none;
-                transition: all .25s ease-in-out;
-                color: #fff;
-                font-size: 12px;
-                padding: 6px 12px;
-                line-height: 1.5;
-                border-radius: 4px;
-
-                &:hover {
-                  background-color: #d2322d;
-                }
-              }
-            }
-
-            footer {
-              background-color: #fff;
-              padding: 10px 15px;
-              font-size: 10pt;
-              font-weight: bold;
-              color: #f00;
-
-              span {
-                color: #3d4450;
-              }
-            }
-          }
-
-          .md5 {
-            .article {
-              color: #3a87ad;
-              background-color: rgba(217, 237, 247, .8);
-            }
-          }
-
-          .blogTag {
-            i {
-              margin-right: .5em;
-            }
-
-            a {
-              opacity: .7;
-
-              &:hover {
-                opacity: 1;
-              }
-            }
-          }
-        }
-      }
     }
 
     section.sub-wrapper {
