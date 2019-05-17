26caf87e65e0c8d07910a8f1c2d4a5fc34e9e83d
dantecsm <fakersfather@qq.com>
2019-05-17 15:42:58 +0800

    解决 build 后 font 路径不正确的 bug，部分解决 element-ui 样式覆盖问题 (modify utils.js, modify style of Besider.vue)

--- src/components/Besider.vue
+++ src/components/Besider.vue
@@ -109,6 +109,15 @@
   }
 </script>
 
+<style>
+  .Besider aside .input-wrapper .el-input__inner {
+    height: 35px;
+    line-height: 35px;
+    background: transparent;
+    box-shadow: inset 0px 1px 1px rgba(0, 0, 0, .075);
+  }
+</style>
+
 <style scoped lang="less">
   .Besider {
     aside {
